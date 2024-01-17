36708536010c37553b6e7327fc567b60f8c1ede2
dewibatista <62663654+dewi-batista@users.noreply.github.com>
2024-01-17 19:53:27 +0100

    added cross-validation for hyperparameter tuning

--- classification_methods/autoencoder_svm.py
+++ classification_methods/autoencoder_svm.py
@@ -4,10 +4,13 @@
 # =============================================================================
 
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
+from sklearn.model_selection import StratifiedKFold
 from sklearn.svm import SVC
 from tensorflow.keras.layers import Input, Dense
 from tensorflow.keras.models import Model
+
 
 # resconstruct 16 x 15 image from 240 x 1 graysacle vector
 def reconstruct_image(image, num, y_pred, j):
@@ -21,37 +24,47 @@ def reconstruct_image(image, num, y_pred, j):
 
 class svm_classifier:
     
-    def __init__(self, train_file, test_file):
-        self.train_df = pd.read_csv(train_file)
-        self.test_df = pd.read_csv(test_file)
-        self.X_train = self.train_df.drop(columns=['label']).values
-        self.y_train = self.train_df.iloc[:, -1].values
-        self.X_test = self.test_df.drop(columns=['label']).values
-        self.y_test = self.test_df.iloc[:, -1].values
+    def __init__(self, X_train, y_train, X_test, y_test, param):
+        self.X_train = X_train
+        self.y_train = y_train
+        self.X_test = X_test
+        self.y_test = y_test
+        
+        # hyperparameters for fine tuning
+        self.epochs = param
 
     def autoencode(self):
         
         # normalise pixel values
-        X_train_norm = self.X_train / 6
-        X_test_norm  = self.X_test  / 6
+        X_train = self.X_train / 6
+        X_test  = self.X_test  / 6
         
-        # reshape feature data frames to 16 by 15
-        X_train_norm = X_train_norm.reshape((len(X_train_norm), 16 * 15))
-        X_test_norm  = X_test_norm.reshape((len(X_test_norm), 16 * 15))
+        # hyperparameters
+        enc_layers = 64
+        dec_layers = 240
+        
+        enc_act_func = 'relu'
+        dec_act_func = 'sigmoid'
+        
+        auto_enc_opt    = 'adam'
+        auto_enc_loss   = 'binary_crossentropy'
+        auto_enc_epochs = self.epochs
+        auto_enc_batchs = 256
     
         # make autoencoder
-        input_img = Input(shape=(240,))
-        encoded = Dense(128, activation='relu')(input_img)
-        decoded = Dense(240, activation='sigmoid')(encoded)
+        input_layer = Input(shape=(240,))
+        encoded = Dense(enc_layers, activation=enc_act_func)(input_layer)
+        decoded = Dense(dec_layers, activation=dec_act_func)(encoded)
         
-        autoencoder  = Model(input_img, decoded)
-        self.encoder = Model(input_img, encoded)
+        autoencoder  = Model(input_layer, decoded)
+        self.encoder = Model(input_layer, encoded)
         
-        autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
-        autoencoder.fit(X_train_norm, X_train_norm, epochs=50, batch_size=256,
-                        shuffle=True, validation_data=(X_test_norm, X_test_norm))
-    
-        encoded = self.encoder.predict(X_test_norm)
+        autoencoder.compile(optimizer=auto_enc_opt, loss=auto_enc_loss)
+        autoencoder.fit(X_train, X_train, epochs=auto_enc_epochs,
+                        batch_size=auto_enc_batchs, shuffle=True,
+                        validation_data=(X_test, X_test), verbose=0)
+
+        encoded = self.encoder.predict(X_test, verbose=0)
         # decoded = autoencoder.predict(X_test_norm)
         
         # test_loss = autoencoder.evaluate(X_test, X_test)
@@ -59,28 +72,30 @@ class svm_classifier:
     
         return encoded
 
-    def svm_pred(self, X_train_encoded, y_train):
+    def svm_pred(self, X_train_enc, y_train):
         
         # define and train SVM model
+        # hyperparameteres here are choice of kernel and regularisation 'C'
         svm_model = SVC()
-        svm_model.fit(X_train_encoded, y_train)
+        # svm_model = SVC(kernel='poly', degree=3)
+        # svm_model = SVC(kernel='rbf', C=10.0)
+        svm_model.fit(X_train_enc, y_train)
     
         # encode test data
-        X_test_encoded = self.encoder.predict(self.X_test / 6)
-    
+        X_test_encoded = self.encoder.predict(self.X_test / 6, verbose=0)
+
         # predict using SVM model
         y_pred = svm_model.predict(X_test_encoded)
-        
+
         return y_pred
         
     def assess_pred(self, X_train, y_train):
-        
-        # make preds
         y_pred = self.svm_pred(X_train, y_train)
+        return (y_pred != self.y_test).sum()
     
-        # print total number of inaccuracies
-        print("\n\nSVM Results ~")
-        print("Total number of incorrect predictions:", (y_pred != self.y_test).sum())
+        # # print total number of inaccuracies
+        # print("\n\nSVM Results ~")
+        # print("Total number of incorrect predictions:", (y_pred != self.y_test).sum())
         
         # # print number of inaccuracies by digit
         # for i in range(10):
@@ -95,14 +110,49 @@ class svm_classifier:
         #         if y_pred[num] != self.y_test[num]:
         #             print(f"image {num} is mistaken for a {y_pred[num]}, should be {j}")
         #             reconstruct_image(self.X_test[num], num, y_pred[num], j)
-
+        
+def cross_validate(num_folds, X_train, y_train, X_test, y_test, param):
+    
+    skf = StratifiedKFold(n_splits=num_folds)
+    for epochs in range(50, param + 1):
+        
+        fold_num_wrong = []
+        for train_index, val_index in skf.split(X_train, y_train):
+            
+            # split data into training and validation folds
+            X_train_fold, X_val_fold = X_train[train_index], X_train[val_index]
+            y_train_fold, y_val_fold = y_train[train_index], y_train[val_index]
+            
+            classifier = svm_classifier(X_train_fold, y_train_fold,
+                                        X_val_fold, y_val_fold, epochs)
+            X_train_enc = classifier.autoencode()
+            fold_num_wrong.append(classifier.assess_pred(X_train_enc,
+                                                         y_val_fold))
+        print(np.mean(fold_num_wrong))
+    
 if __name__ == "__main__":
     
-    # initalise svm classifier
-    svm_classifier = svm_classifier('../raw_train.csv', '../raw_test.csv')
+    train_df = pd.read_csv('../raw_train.csv')
+    test_df = pd.read_csv('../raw_test.csv')
     
-    # autoencode data
-    X_train_encoded = svm_classifier.autoencode()
+    # shuffle data so that it doesn't try to learn order of digits
+    # this is somewhat out of paranoia but it doesn't hurt to do
+    train_df = train_df.sample(frac=1)
+    test_df  = test_df.sample(frac=1)
     
-    # assess predictions
-    svm_classifier.assess_pred(X_train_encoded, svm_classifier.y_train)
+    X_train = train_df.drop(columns=['label']).values
+    y_train = train_df.iloc[:, -1].values
+    X_test  = test_df.drop(columns=['label']).values
+    y_test  = test_df.iloc[:, -1].values
+    
+    epochs_max = 55
+    
+    # ten-fold cross-validation
+    cross_validate(10, X_train, y_train, X_test, y_test, epochs_max)
+    
+    # # individual instance
+    # classifier = svm_classifier(X_train, y_train,
+    #                             X_test, y_test, epochs_max)
+    # X_train_enc = classifier.autoencode()
+    # print(classifier.assess_pred(X_train_enc, y_test))
+    
