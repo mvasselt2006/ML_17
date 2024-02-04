6cce2f084a24599006c0ae95383e710c45dfada2
mvasselt2006 <65347380+mvasselt2006@users.noreply.github.com>
2024-02-04 18:30:55 +0100

    Got rid of some redundant code

--- classification_methods/CNN.py
+++ classification_methods/CNN.py
@@ -20,6 +20,10 @@ test_labels = test_df.iloc[:, -1]
 
 class_names = [0,1,2,3,4,5,6,7,8,9]
 
+# =====================================
+# Plotting images
+# =====================================
+
 # plt.figure(figsize=(10,10))
 # for i in range(10):
 #     plt.subplot(5,2,i+1)
@@ -42,6 +46,7 @@ for i in range(1000):
     test_img[i,:,:] = np.array(test_images.iloc[i,:]).reshape(16,15)
 
 def CNN1():
+    """"Basic test"""
     CNN = models.Sequential()
     CNN.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(16, 15, 1)))
     CNN.add(layers.MaxPooling2D((2, 2)))
@@ -59,6 +64,7 @@ def CNN1():
     return CNN
 
 def CNN2():
+    """Model that returned the best results"""
     CNN = models.Sequential()
     CNN.add(layers.Conv2D(32, (5, 5), activation='relu', input_shape=(16, 15, 1), padding='same'))
     CNN.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(16, 15, 1), padding='same'))
@@ -110,10 +116,12 @@ def CNN3():
     return CNN
 
 def training(train_dat,train_label, test_dat, test_label,epoch):
+    """Trains the model with the training data. To change which model to run this
+    for, change the first line of this function to the wanted CNN model"""
     model= CNN2()
     history = model.fit(train_dat, train_label, epochs=epoch, 
                         validation_data=(test_dat, test_label))
-    model.save("Model4.keras")
+    #model.save("Model4.keras")
     plt.plot(history.history['accuracy'], label='accuracy')
     plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
     plt.xlabel('Epoch')
@@ -127,6 +135,7 @@ def training(train_dat,train_label, test_dat, test_label,epoch):
 
 
 def get_cfmatrix(labels,images,model):
+    """Plots the confusion matrix for the model compared to the test labels"""
     pred_labels = model.predict(np.reshape(images,(1000,16,15,1)))
     pred = (np.argmax(pred_labels, axis=1)).flatten()
    
@@ -136,49 +145,51 @@ def get_cfmatrix(labels,images,model):
     plt.show()
 
 def shuffle_data(train_dat,train_label,test_dat,test_label):
+    """"Shuffles the dataset and splits the train and test data randomly"""
     pics = np.concatenate(train_dat, test_dat)
     labels= np.concatenate(train_label,test_label)
     train_pics, test_pics, train_labels, test_labels = train_test_split(pics, labels, test_size = 0.5, shuffle=True)
     return train_pics, test_pics, train_labels, test_labels
 
-# load data frames
-train_df = pd.read_csv(f"rot_upto25.csv")
+def print_misclass(model,x_size, y_size):
+    """Plots all the incorrectely labeled test images for the model. These images are plotted on a 
+    grid of (x_size, y_size) images"""
+    pred_labels = model.predict(np.reshape(test_img,(1000,16,15,1)))
+    pred = (np.argmax(pred_labels, axis=1)).flatten()
+    i_incorrect=[]
+    for i in range(1000):
+        if pred[i]-test_labels[i] != 0:
+            i_incorrect.append(i)
 
-# split training data into features and label
-train_images = train_df.iloc[:, :-1]
-train_labels = train_df.iloc[:, -1]
+    print(i_incorrect)
+    plt.figure(figsize=(20,15))
+    for i in range(len(i_incorrect)):
+        plt.subplot(x_size,y_size,i+1)
+        plt.xticks([])
+        plt.yticks([])
+        plt.grid(False)
+        ind= i_incorrect[i]
+        img= np.array(test_images.iloc[ind,:])
+        plt.imshow(img.reshape((16,15)),cmap='gray')
+        # The CIFAR labels happen to be arrays, 
+        # which is why you need the extra index
+        plt.xlabel(f"True {test_labels[ind]}, pred {pred[ind]}")
+    plt.show()
 
-#make images into correct shape
-train_img= np.zeros((5000,16,15))
-for i in range(5000):
-    train_img[i,:,:] = np.array(train_images.iloc[i,:]).reshape(16,15)
 
-new_mod= training(train_img, train_labels, test_img, test_labels,15)
-get_cfmatrix(test_labels, test_img, new_mod) 
+#Trains a new model
+#mod= training(train_img, train_labels, test_img, test_labels,15)
+#Loads a trained model
+mod = tf.keras.models.load_model('model3.keras')
 
-#mod = tf.keras.models.load_model('model4.keras')
-pred_labels = new_mod.predict(np.reshape(test_img,(1000,16,15,1)))
-pred = (np.argmax(pred_labels, axis=1)).flatten()
-i_incorrect=[]
-for i in range(1000):
-    if pred[i]-test_labels[i] != 0:
-        i_incorrect.append(i)
+#Gets confusion matrix
+#get_cfmatrix(test_labels, test_img, mod) 
+print_misclass(mod,4,5)
 
-print(i_incorrect)
-plt.figure(figsize=(20,15))
-for i in range(len(i_incorrect)):
-    plt.subplot(5,4,i+1)
-    plt.xticks([])
-    plt.yticks([])
-    plt.grid(False)
-    ind= i_incorrect[i]
-    img= np.array(test_images.iloc[ind,:])
-    plt.imshow(img.reshape((16,15)))
-    # The CIFAR labels happen to be arrays, 
-    # which is why you need the extra index
-    plt.xlabel(f"True {test_labels[ind]}, pred {pred[ind]}")
-plt.show()
 
+# =============================================
+# Iterates over different augmented datasets
+# =============================================
    
 # acc_list=[]
 # model= CNN2()
